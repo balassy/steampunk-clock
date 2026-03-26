6a9494b4560a08e335a10996a387cbca63191a09
György Balássy <balassy@users.noreply.github.com>
2026-03-26 16:35:12 +0100

    doc: updated wiring to ESP32

--- config.h
+++ config.h
@@ -1,20 +1,20 @@
 #pragma once
 
 // Pin configuration for the ESP32-WROOM-32 board.
-inline constexpr uint8_t PIN_LED = 2;           // GPIO2 onboard LED
-inline constexpr uint8_t PIN_HOUR_LED = 12;     // GPIO12 hour display LED (+)
-inline constexpr uint8_t PIN_MINUTE_LED = 13;   // GPIO13 minute display LED (+)
-inline constexpr uint8_t PIN_RTC_SDA = 21;      // GPIO21 SDA for RTC
-inline constexpr uint8_t PIN_RTC_SCL = 22;      // GPIO22 SCL for RTC
-inline constexpr uint8_t PIN_SERVO_HOUR = 16;   // GPIO16 hour servo
-inline constexpr uint8_t PIN_SERVO_MINUTE = 17; // GPIO17 minute servo
-inline constexpr uint8_t PIN_BUZZER = 20;       // GPIO20 active buzzer (+)
+inline constexpr uint8_t PIN_LED = 2;             // GPIO2 onboard LED
+inline constexpr uint8_t PIN_HOUR_LED = 12;       // GPIO12 hour display LED (+, - --> GND)
+inline constexpr uint8_t PIN_MINUTE_LED = 13;     // GPIO13 minute display LED (+, - --> GND)
+inline constexpr uint8_t PIN_RTC_SDA = 21;        // GPIO21 SDA for RTC
+inline constexpr uint8_t PIN_RTC_SCL = 22;        // GPIO22 SCL for RTC
+inline constexpr uint8_t PIN_SERVO_HOUR = 16;     // GPIO16 hour servo
+inline constexpr uint8_t PIN_SERVO_MINUTE = 17;   // GPIO17 minute servo
+inline constexpr uint8_t PIN_BUZZER = 23;         // GPIO23 active buzzer (+, - --> GND)
 
-inline constexpr uint8_t PIN_SETTINGS_BUTTON = 4; // GPIO4Button for resetting network settings. 
+inline constexpr uint8_t PIN_SETTINGS_BUTTON = 4; // GPIO4 Push button for resetting network settings (--> GND). 
 
-inline constexpr uint8_t PIN_SWITCH_MODE_1 = 25;
-inline constexpr uint8_t PIN_SWITCH_MODE_2 = 26;
-inline constexpr uint8_t PIN_SWITCH_MODE_3 = 27;
+inline constexpr uint8_t PIN_SWITCH_MODE_1 = 25;  // GPIO25 switch mode 1 (--> 3.3V)
+inline constexpr uint8_t PIN_SWITCH_MODE_2 = 26;  // GPIO26 switch mode 2 (--> 3.3V)
+inline constexpr uint8_t PIN_SWITCH_MODE_3 = 27;  // GPIO27 switch mode 3 (--> 3.3V)
 
 
 // Hardware settings.
